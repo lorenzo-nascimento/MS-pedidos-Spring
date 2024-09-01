9d25dff0fc98fa590e19016b6e2cf523e0c49866
lorenzo-nascimento <lorenzonascimento.dev@gmail.com>
2024-09-01 17:28:07 -0300

    Upload do projeto no repo remoto

---
+++ ms-pedidos_aula33_parte_1_codigo/src/main/java/br/com/fiap/ms_pedidos/model/ItemDoPedido.java
@@ -0,0 +1,29 @@
+package br.com.fiap.ms_pedidos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_item_do_pedido")
+public class ItemDoPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantidade;
+    private String descricao;
+}
